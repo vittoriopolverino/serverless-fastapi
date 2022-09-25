9e86633cbd4ee9e79d3f7cf2d0625faa4e1fd617
Vittorio Polverino <vittorio.polverino@brumbrum.it>
2022-09-25 21:33:06 +0200

    serverless-fastapi

---
+++ app/api/v1/endpoints/users.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+router = APIRouter()
+
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    is_registered: Union[bool, None] = None
+
+
+@router.get("/")
+async def get_users():
+    return {"message": "Users!"}
+
+
+@router.put("/user/{user_id}")
+def update_user(user_id: int, user: User):
+    return {"user_firstname": user.first_name, "user_id": user_id}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Run FastAPI
+pipenv run uvicorn app.main:app --reload
